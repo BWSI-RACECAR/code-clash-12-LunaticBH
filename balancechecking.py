64d25ba63a8b503ef8b925cdde5fd9ae60dd0c9e
Enzo C <125571218+LunaticBH@users.noreply.github.com>
2023-07-27 09:31:07 -0400

    Add files via upload

--- balancechecking.py
+++ balancechecking.py
@@ -34,7 +34,7 @@ class Solution:
             length = len(parenthesis)
             num = -1
             if length % 2 == 0 and parenthesis[0] == "[" or parenthesis[0] == "{" or parenthesis[0] == "(":
-                for i in range(length/2):
+                for i in range(int(length/2)):
                     if parenthesis[num] == dict(parenthesis[i]):
                         num -= 1
                     else:
